c5d3070aae9a80788f69c3f019583b3e8f2c44d4
alvindrakes <alvinlohjinxian@gmail.com>
2019-08-14 21:11:40 +0800

    cactus defender can now shoot

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject gun;
+
+
+    // Start is called before the first frame update
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+    }
+
+   
+}
