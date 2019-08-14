4d595d43080929813d90b7e0fdd01735d15c3661
alvindrakes <alvinlohjinxian@gmail.com>
2019-08-14 22:10:06 +0800

    cactus defender can now shoot projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] [Range(1f, 10f)] float speed = 1f;
+  //  [SerializeField] [Range(360f, 7200f)] float rotation = 360f;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.Translate(Vector2.right *  speed * Time.deltaTime);
+    // transform.Rotate(Vector3.forward, rotation * Time.deltaTime);
+
+    }
+
+}
