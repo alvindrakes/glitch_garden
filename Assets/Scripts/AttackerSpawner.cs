b3de7c628b6a89345b2ed0b6bd698788dcdda0e6
alvindrakes <alvinlohjinxian@gmail.com>
2019-08-17 13:15:22 +0800

    lizard has health and projectile deal damage

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -32,3 +32,4 @@ public class AttackerSpawner : MonoBehaviour
         
     }
 }
+
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,6 +6,8 @@ public class Projectile : MonoBehaviour
 {
 
     [SerializeField] [Range(1f, 10f)] float speed = 1f;
+    [SerializeField] [Range(1f, 100f)] float damage = 50f;
+
   //  [SerializeField] [Range(360f, 7200f)] float rotation = 360f;
 
     // Start is called before the first frame update
@@ -14,6 +16,16 @@ public class Projectile : MonoBehaviour
         transform.Translate(Vector2.right *  speed * Time.deltaTime);
     // transform.Rotate(Vector3.forward, rotation * Time.deltaTime);
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+     {
+
+        var health = otherCollider.GetComponent<Health>();
+
+        Debug.Log("I hit " + otherCollider.name);
+        // reduce health of attacker 
+        health.dealDamage(damage);
     }
 
 }
