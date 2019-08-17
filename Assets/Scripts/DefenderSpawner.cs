da8d72c0940c1513d19706c09da3508ad2bd9625
alvindrakes <alvinlohjinxian@gmail.com>
2019-08-17 14:37:18 +0800

    defender can now spawn based on mouse click position

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+
+    private void OnMouseDown() {
+        SpawnDefender(getClickPosition());
+    }
+
+    private Vector2 getClickPosition() 
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+
+    void SpawnDefender(Vector2 worldPos)
+    {
+        GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity) as GameObject; 
+    }
+}
