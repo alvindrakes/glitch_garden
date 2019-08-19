d8992f9801fd2b98b0eb147f07f7884859605095
alvindrakes <alvinlohjinxian@gmail.com>
2019-08-19 17:38:20 +0800

    attacker attack when there is defender and stop when not

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -7,15 +7,43 @@ public class Attacker : MonoBehaviour
 
     [Range (0f, 5f)]
     float currentSpeed = 1f;
+    GameObject currentTarget;
 
 
       void Update()
     {
-        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);        
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
     }
 
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) { return;  }
+
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.dealDamage(damage);
+        } 
+    }
+
+    // if the defender is dead, attacker continue walking 
+    private void UpdateAnimationState()
+    {
+        if(!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("isAttacking", false);
+        }
     }
 }
